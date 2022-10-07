12656914d0a7942d224ee97691d9f38d99258718
Ethan Billingsley <ethanjb98.eb@gmail.com>
2022-10-06 19:30:43 -0600

    added method to calculate pi

--- main.cpp
+++ main.cpp
@@ -1,8 +1,15 @@
 #include "System.h"
+#include <cmath>
+#include <iostream>
+#include <iomanip>
 
 int main()
 {
 	System system{};
+
+	const double pi{ 4.0 * std::atan(1.0) };
+
+	std::cout << std::setprecision(15) << std::fixed << pi << '\n';
 
 	return 0;
 }
