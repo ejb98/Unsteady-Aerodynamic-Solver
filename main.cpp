5d6fb6d345ecac23ce9a38aa7289aaf22b45fd7b
Ethan Billingsley <ethanjb98.eb@gmail.com>
2022-10-07 15:55:17 -0600

    added statement to main

--- main.cpp
+++ main.cpp
@@ -1,20 +1,23 @@
 #include "Mesh.h"
 #include "Surface.h"
 #include "constructRectangularMesh.h"
+#include <iostream>
 
 int main()
 {
 	double chord{ 1.0 };
 	double span{ 6.0 };
 
-	int chordwisePanels{ 2 };
-	int spanwisePanels{ 2 };
+	int chordwisePanels{ 12 };
+	int spanwisePanels{ 10 };
 
 	Mesh mesh{ constructRectangularMesh(chord, span, chordwisePanels, spanwisePanels) };
 
 	Surface surface{ mesh };
 
-	surface.print();
+	//surface.print();
+
+	std::cout << "Surface created with " << chordwisePanels * spanwisePanels << " Panels.\n";
 
 	return 0;
 }
